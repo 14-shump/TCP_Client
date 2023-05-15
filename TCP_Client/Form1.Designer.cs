155eef598855937126c4178d97be0bb17ee2b6d2
14-shump <syun2314@gmail.com>
2023-05-15 09:10:11 +0900

    first commit

---
+++ TCP_Client/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace TCP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Receive = new System.Windows.Forms.TextBox();
+            this.lbl_Receive = new System.Windows.Forms.Label();
+            this.txt_Cmd = new System.Windows.Forms.TextBox();
+            this.lbl_Cmd = new System.Windows.Forms.Label();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.lbl_IP = new System.Windows.Forms.Label();
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.lbl_Port = new System.Windows.Forms.Label();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_Receive
+            // 
+            this.txt_Receive.Location = new System.Drawing.Point(12, 98);
+            this.txt_Receive.Multiline = true;
+            this.txt_Receive.Name = "txt_Receive";
+            this.txt_Receive.ReadOnly = true;
+            this.txt_Receive.Size = new System.Drawing.Size(475, 138);
+            this.txt_Receive.TabIndex = 0;
+            // 
+            // lbl_Receive
+            // 
+            this.lbl_Receive.AutoSize = true;
+            this.lbl_Receive.Location = new System.Drawing.Point(12, 83);
+            this.lbl_Receive.Name = "lbl_Receive";
+            this.lbl_Receive.Size = new System.Drawing.Size(53, 12);
+            this.lbl_Receive.TabIndex = 1;
+            this.lbl_Receive.Text = "受信内容";
+            // 
+            // txt_Cmd
+            // 
+            this.txt_Cmd.Location = new System.Drawing.Point(14, 61);
+            this.txt_Cmd.Name = "txt_Cmd";
+            this.txt_Cmd.Size = new System.Drawing.Size(367, 19);
+            this.txt_Cmd.TabIndex = 2;
+            // 
+            // lbl_Cmd
+            // 
+            this.lbl_Cmd.AutoSize = true;
+            this.lbl_Cmd.Location = new System.Drawing.Point(12, 46);
+            this.lbl_Cmd.Name = "lbl_Cmd";
+            this.lbl_Cmd.Size = new System.Drawing.Size(40, 12);
+            this.lbl_Cmd.TabIndex = 3;
+            this.lbl_Cmd.Text = "コマンド";
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(387, 61);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(100, 19);
+            this.btn_Send.TabIndex = 4;
+            this.btn_Send.Text = "送信";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(387, 24);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(100, 19);
+            this.btn_Connect.TabIndex = 7;
+            this.btn_Connect.Text = "接続";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // lbl_IP
+            // 
+            this.lbl_IP.AutoSize = true;
+            this.lbl_IP.Location = new System.Drawing.Point(12, 9);
+            this.lbl_IP.Name = "lbl_IP";
+            this.lbl_IP.Size = new System.Drawing.Size(51, 12);
+            this.lbl_IP.TabIndex = 6;
+            this.lbl_IP.Text = "IPアドレス";
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(14, 24);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(261, 19);
+            this.txt_IP.TabIndex = 5;
+            this.txt_IP.Text = "192.168.10.107";
+            // 
+            // lbl_Port
+            // 
+            this.lbl_Port.AutoSize = true;
+            this.lbl_Port.Location = new System.Drawing.Point(279, 9);
+            this.lbl_Port.Name = "lbl_Port";
+            this.lbl_Port.Size = new System.Drawing.Size(33, 12);
+            this.lbl_Port.TabIndex = 9;
+            this.lbl_Port.Text = "ポート";
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(281, 24);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(100, 19);
+            this.txt_Port.TabIndex = 8;
+            this.txt_Port.Text = "4000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 250);
+            this.Controls.Add(this.lbl_Port);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.lbl_IP);
+            this.Controls.Add(this.txt_IP);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.lbl_Cmd);
+            this.Controls.Add(this.txt_Cmd);
+            this.Controls.Add(this.lbl_Receive);
+            this.Controls.Add(this.txt_Receive);
+            this.Name = "Form1";
+            this.Text = "TCP_Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Receive;
+        private System.Windows.Forms.Label lbl_Receive;
+        private System.Windows.Forms.TextBox txt_Cmd;
+        private System.Windows.Forms.Label lbl_Cmd;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Label lbl_IP;
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.Label lbl_Port;
+        private System.Windows.Forms.TextBox txt_Port;
+    }
+}
+
---
+++ TCP_Client/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Client
+{
+    public partial class Form1 : Form
+    {
+        public IPEndPoint ServerIPEndPoint { get; set; }
+        private Socket Socket { get; set; }
+        public const int BufferSize = 1024;
+        public byte[] Buffer { get; } = new byte[BufferSize];
+
+        /// <summary>
+        /// STX
+        /// </summary>
+        private const char _stx = '\x02';
+
+        /// <summary>
+        /// ETX
+        /// </summary>
+        private const char _etx = '\x03';
+
+        /// <summary>
+        /// 受信メッセージ
+        /// </summary>
+        private string _receiveMsg = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ソケット通信の接続
+        public void Connect()
+        {
+            this.Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            this.Socket.Connect(this.ServerIPEndPoint);
+
+            // 非同期で受信を待機
+            this.Socket.BeginReceive(this.Buffer, 0, BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), this.Socket);
+        }
+
+        // ソケット通信接続の切断
+        public void DisConnect()
+        {
+            this.Socket?.Disconnect(false);
+            this.Socket?.Dispose();
+        }
+
+        // 非同期受信のコールバックメソッド(別スレッドで実行される)
+        private void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            var socket = asyncResult.AsyncState as Socket;
+
+            var byteSize = -1;
+            try
+            {
+                // 受信を待機
+                byteSize = socket.EndReceive(asyncResult);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            // 受信したデータがある場合、その内容を表示する
+            // 再度非同期での受信を開始する
+            if (byteSize > 0)
+            {
+                byte[] buff = new byte[byteSize];
+                buff = Encoding.GetEncoding("ASCII").GetBytes(Encoding.ASCII.GetString(this.Buffer, 0, byteSize));
+
+                // 初期化
+                bool isStx = false;
+                bool isEtx = false;
+                int posStart = 0;
+                int posEnd = byteSize - 1;
+
+                for (int i = 0; i < buff.Length; i++)
+                {
+                    if (!isStx && buff[i] == _stx)
+                    {
+                        // STXがあればその次の位置を開始インデックスにする
+                        if (isEtx) isEtx = false;
+                        posStart = i + 1;
+                        isStx = true;
+                    }
+                    if (!isEtx && buff[i] == _etx)
+                    {
+                        // ETXがあればその前の位置を終了インデックスにする
+                        posEnd = i - 1;
+                        isEtx = true;
+                    }
+                }
+
+                // STXがあれば溜めていた受信メッセージをクリアする
+                if (isStx) _receiveMsg = string.Empty;
+
+                // ETXがなければ終了インデックスを末尾にする
+                if (!isEtx) posEnd = byteSize - 1;
+
+                if (posStart < byteSize || posEnd >= 0)
+                {
+                    // 開始・終了インデックスが範囲内の場合
+                    byte[] tmpBuff = new byte[posEnd - posStart + 1];
+
+                    // 開始から終了インデックスの範囲のデータを文字列変換用のバッファにコピーする
+                    Array.Copy(buff, posStart, tmpBuff, 0, tmpBuff.Length);
+
+                    // ASCII文字列としてエンコードして溜めていた受信メッセージに連結する(送信する内容に合わせて下さい)
+                    _receiveMsg += Encoding.ASCII.GetString(tmpBuff);
+                }
+
+                if (isEtx)
+                {
+                    this.Invoke((Action)(() => { this.txt_Receive.Text = _receiveMsg; }));
+                }
+
+                socket.BeginReceive(this.Buffer, 0, this.Buffer.Length, SocketFlags.None, ReceiveCallback, socket);
+            }
+        }
+
+
+        /// <summary>
+        /// FormClosing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DisConnect();
+        }
+
+        /// <summary>
+        /// 接続ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            this.ServerIPEndPoint = new IPEndPoint(IPAddress.Parse(this.txt_IP.Text), Int32.Parse(this.txt_Port.Text));
+            this.Connect();
+        }
+
+        /// <summary>
+        /// 送信ボタンクリック
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            var sendBytes = new UTF8Encoding().GetBytes(this.txt_Cmd.Text);
+            this.Socket.Send(sendBytes);
+        }
+    }
+}
